a0e6cbf17ad85057d3c46e14f0d5633370323447
omri yoffe <omriyoffe366@gmail.com>
2020-09-20 23:29:28 +0300

    removed src folder

--- src/js/models/Drink.js
+++
@@ -1,9 +0,0 @@
-export default class Drink {
-	constructor(id, name, img, instructions = "", ingredients = []) {
-		this.id = id;
-		this.name = name;
-		this.img = img;
-		this.instructions = instructions;
-		this.ingredients = ingredients;
-	}
-}
